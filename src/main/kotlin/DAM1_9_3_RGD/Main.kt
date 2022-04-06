4572c2f5c2a064b287bf38ca768468480b3f8248
Ricardo <rgallego@iesrafaelalberti.es>
2022-04-06 21:20:33 +0200

    9.3 FALTA ARREGLAR RESTRICCIÓN

--- src/main/kotlin/DAM1_9_3_RGD/Main.kt
+++ src/main/kotlin/DAM1_9_3_RGD/Main.kt
@@ -1,7 +1,5 @@
 package DAM1_9_3_RGD
 
-import SOBRAS.DAM1_9_2_RGD.LibrosDAO
-import java.sql.Date
 import java.sql.DriverManager
 
 data class Tiendas(
@@ -9,11 +7,11 @@ data class Tiendas(
 )
 
 data class Inventarios(
-    val id_articulo: Int,
-    val Nombre: String,
-    val Comentario: String,
+    var id_articulo: Int,
+    var Nombre: String,
+    var Comentario: String,
     var Precio: Double,
-    val id_tienda: Int,
+    var id_tienda: Int,
 )
 
 fun main() {
@@ -22,13 +20,6 @@ fun main() {
     val username = "programacion"
     val password = "programacion"
     val inventariosDAO = InventariosDAO(jdbcUrl, username, password)
-
-    /*
-    var contador = 0
-    inventariosDAO.selectAll().forEach { contador++ }
-    println(contador)
-    */
-
 
     val connection = DriverManager.getConnection(jdbcUrl, username, password)
 
@@ -47,18 +38,7 @@ fun main() {
             inventariosDAO.selectAll().forEach { limiteInventarios++ }
             tiendasDAO.selectAll().forEach { limiteTiendas++ }
 
-            //Hago un bucle en el que reviso cada fila y compruebo que cumplan la condicion para actualizarlo
-            /*
-            for (i in 1..limiteInventarios) {
-                var b = inventariosDAO.selectById(i)
-
-                if (b != null && b.Precio > 2000) {
-                    b.Precio = b.Precio * 1.15
-                    inventariosDAO.update(b)
-                }
-            }
-             */
-            //Intento hacerlo de otra manera para que la excepción no salte pero sigue
+            //Compruebo si el precio es mayor al indicado para aplicarle el porcentaje
             inventariosDAO.selectAll().forEach {
                 if (it.Precio > 2000) {
                     it.Precio = it.Precio * 1.15; inventariosDAO.update(it)
@@ -67,17 +47,17 @@ fun main() {
 
             //Muestro los inventarios por tiendas
             for (k in 1..limiteTiendas) {
-                var a = tiendasDAO.selectById(k)
+                var objetoTiendaPorID = tiendasDAO.selectById(k)
 
                 //Para cada fila de tiendasDAO imprimo todas las filsa de Inventario que correspondan con su ID
                 println("=======================================")
-                println(a)
+                println(objetoTiendaPorID)
                 for (i in 1..6) {
-                    var b = inventariosDAO.selectById(i)
-                    if (b != null) {
-                        if (b.id_tienda == k) {
+                    var objetoInventarioPorID = inventariosDAO.selectById(i)
+                    if (objetoInventarioPorID != null) {
+                        if (objetoInventarioPorID.id_tienda == k) {
                             println("---------------------------------------")
-                            println(b)
+                            println(objetoInventarioPorID)
                             println("---------------------------------------")
                         }
                     }
